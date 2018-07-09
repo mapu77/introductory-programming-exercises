3ea5d8927e3d8ed6c97f69ea880d926fd9b97d6c
euadelgado <albadelgadoc@gmail.com>
2018-07-09 15:19:10 +0200

    solved prime factors of 1

--- src/main/java/edu/thoughtworks/training/PrimeFactor.java
+++ src/main/java/edu/thoughtworks/training/PrimeFactor.java
@@ -1,9 +1,12 @@
 package edu.thoughtworks.training;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class PrimeFactor {
     public Collection<Integer> generate(int input) {
-        return null;
+        Collection<Integer> primeFactors = new ArrayList<Integer>();
+
+        return primeFactors;
     }
 }
