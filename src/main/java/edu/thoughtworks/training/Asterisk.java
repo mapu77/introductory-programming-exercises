8c744bde5f92cee723076710c494ccb086d3ac28
euadelgado <albadelgadoc@gmail.com>
2018-07-09 11:16:27 +0200

    add vertical line feature and rename previous methods

--- src/main/java/edu/thoughtworks/training/Asterisk.java
+++ src/main/java/edu/thoughtworks/training/Asterisk.java
@@ -4,17 +4,19 @@ public class Asterisk {
 
     private static final String ASTERISK = "*";
 
-    public String print() {
+    public String printAnAsterisk() {
         return ASTERISK;
     }
 
-    public String print(int n) {
+    public String printHorizontalLine(int n) {
         StringBuilder builder = new StringBuilder();
-        for (int i=0; i<n; ++i) builder.append(print());
+        for (int i = 0; i < n; ++i) builder.append(printAnAsterisk());
         return builder.toString();
     }
 
     public String printVerticalLine(int n) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < n; ++i) builder.append(printAnAsterisk()).append("\n");
+        return builder.toString();
     }
 }
--- src/test/java/edu/thoughtworks/training/AsteriskShould.java
+++ src/test/java/edu/thoughtworks/training/AsteriskShould.java
@@ -8,14 +8,14 @@ public class AsteriskShould {
     @Test
     public void printAnAsterisk() {
         Asterisk asterisk = new Asterisk();
-        assertEquals("Asterisk is not returning an asterisk", "*", asterisk.print());
+        assertEquals("Asterisk is not returning an asterisk", "*", asterisk.printAnAsterisk());
     }
 
     @Test
     public void printANumberOfAsterisk() {
         Asterisk asterisk = new Asterisk();
         int n = 6;
-        assertEquals("Asterisk prints less than two asterisks", "******", asterisk.print(n));
+        assertEquals("Asterisk prints less than two asterisks", "******", asterisk.printHorizontalLine(n));
     }
 
     @Test
