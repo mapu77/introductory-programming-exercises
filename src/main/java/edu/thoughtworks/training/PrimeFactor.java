7b78bd15ba1f98246c27bd6f52333ffd0c7936db
euadelgado <albadelgadoc@gmail.com>
2018-07-09 15:34:24 +0200

    solved prime factors

--- src/main/java/edu/thoughtworks/training/PrimeFactor.java
+++ src/main/java/edu/thoughtworks/training/PrimeFactor.java
@@ -6,6 +6,14 @@ import java.util.Collection;
 public class PrimeFactor {
     public Collection<Integer> generate(int input) {
         Collection<Integer> primeFactors = new ArrayList<Integer>();
+        for (int i = 2; i <= Math.sqrt(input); ++i) {
+            if (input % i == 0) {
+                primeFactors.add(i);
+                input /= i;
+            }
+
+        }
+        if (input != 1) primeFactors.add(input);
 
         return primeFactors;
     }
