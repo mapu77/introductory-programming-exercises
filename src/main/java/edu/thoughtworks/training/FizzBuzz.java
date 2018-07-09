abc8cd543f703ad15b9ebc18f78bc204262773f8
Eduard Maura <edu.maura.puig@gmail.com>
2018-07-09 14:47:16 +0200

    Implement normal representation case and add FizzBuzz 1 to 100 test case

--- src/main/java/edu/thoughtworks/training/FizzBuzz.java
+++ src/main/java/edu/thoughtworks/training/FizzBuzz.java
@@ -2,9 +2,26 @@ package edu.thoughtworks.training;
 
 public class FizzBuzz {
     public String represent(int number) {
-        String representation = new String();
-        if (number % 3 == 0) representation += "Fizz";
-        if (number % 5 == 0) representation += "Buzz";
+        String representation = "";
+        if (isDivisibleBy3(number)) representation += "Fizz";
+        if (isDivisibleBy5(number)) representation += "Buzz";
+        if (!isDivisibleBy3(number) && !isDivisibleBy5(number)) representation = String.valueOf(number);
         return representation;
+    }
+
+    private boolean isDivisibleBy5(int number) {
+        return isDivisibleBy(number, 5);
+    }
+
+    private boolean isDivisibleBy3(int number) {
+        return isDivisibleBy(number, 3);
+    }
+
+    private boolean isDivisibleBy(int number, int i) {
+        return number % i == 0;
+    }
+
+    public String fizzBuzz() {
+        return null;
     }
 }
--- src/test/java/edu/thoughtworks/training/FizzBuzzShould.java
+++ src/test/java/edu/thoughtworks/training/FizzBuzzShould.java
@@ -6,6 +6,109 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FizzBuzzShould {
+
+    public static final String FIZZ_BUZZ_RESULT =
+            "1\n" +
+            "2\n" +
+            "Fizz\n" +
+            "4\n" +
+            "Buzz\n" +
+            "Fizz\n" +
+            "7\n" +
+            "8\n" +
+            "Fizz\n" +
+            "Buzz\n" +
+            "11\n" +
+            "Fizz\n" +
+            "13\n" +
+            "14\n" +
+            "FizzBuzz\n" +
+            "16\n" +
+            "17\n" +
+            "Fizz\n" +
+            "19\n" +
+            "Buzz\n" +
+            "Fizz\n" +
+            "22\n" +
+            "23\n" +
+            "Fizz\n" +
+            "Buzz\n" +
+            "26\n" +
+            "Fizz\n" +
+            "28\n" +
+            "29\n" +
+            "FizzBuzz\n" +
+            "31\n" +
+            "32\n" +
+            "Fizz\n" +
+            "34\n" +
+            "Buzz\n" +
+            "Fizz\n" +
+            "37\n" +
+            "38\n" +
+            "Fizz\n" +
+            "Buzz\n" +
+            "41\n" +
+            "Fizz\n" +
+            "43\n" +
+            "44\n" +
+            "FizzBuzz\n" +
+            "46\n" +
+            "47\n" +
+            "Fizz\n" +
+            "49\n" +
+            "Buzz\n" +
+            "Fizz\n" +
+            "52\n" +
+            "53\n" +
+            "Fizz\n" +
+            "Buzz\n" +
+            "56\n" +
+            "Fizz\n" +
+            "58\n" +
+            "59\n" +
+            "FizzBuzz\n" +
+            "61\n" +
+            "62\n" +
+            "Fizz\n" +
+            "64\n" +
+            "Buzz\n" +
+            "Fizz\n" +
+            "67\n" +
+            "68\n" +
+            "Fizz\n" +
+            "Buzz\n" +
+            "71\n" +
+            "Fizz\n" +
+            "73\n" +
+            "74\n" +
+            "FizzBuzz\n" +
+            "76\n" +
+            "77\n" +
+            "Fizz\n" +
+            "79\n" +
+            "Buzz\n" +
+            "Fizz\n" +
+            "82\n" +
+            "83\n" +
+            "Fizz\n" +
+            "Buzz\n" +
+            "86\n" +
+            "Fizz\n" +
+            "88\n" +
+            "89\n" +
+            "FizzBuzz\n" +
+            "91\n" +
+            "92\n" +
+            "Fizz\n" +
+            "94\n" +
+            "Buzz\n" +
+            "Fizz\n" +
+            "97\n" +
+            "98\n" +
+            "Fizz\n" +
+            "Buzz";
+
     @Test
     public void returnFizzWhenNumberIsDivisibleBy3() {
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -33,5 +136,12 @@ public class FizzBuzzShould {
         assertThat("FizzBuzz doesn't return number when number is not divisible by 3 or 5",
                 fizzBuzz.represent(8), is("8"));
 
+    }
+
+    @Test
+    public void return1to100FizzBuzzNumbers() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat("FizzBuzz doesn't work",
+                fizzBuzz.fizzBuzz(), is(FIZZ_BUZZ_RESULT));
     }
 }
